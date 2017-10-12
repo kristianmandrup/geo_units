322f57eaae199fc3147e7c4dd823851227f61718
Kristian Mandrup <kmandrup@gmail.com>
2017-10-12 14:04:07 +0100

    all tests pass

--- lib/geo_units.rb
+++ lib/geo_units.rb
@@ -1,20 +1,21 @@
- # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+ # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  #  Geodesy representation conversion functions (c) Chris Veness 2002-2010
  #   - www.movable-type.co.uk/scripts/latlong.html
  #
- # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+ # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Parses string representing degrees/minutes/seconds into numeric degrees
-# 
+#
 # This is very flexible on formats, allowing signed decimal degrees, or deg-min-sec optionally
-# suffixed by compass direction (NSEW). A variety of separators are accepted (eg 3º 37' 09"W) 
-# or fixed-width format without separators (eg 0033709W). Seconds and minutes may be omitted. 
+# suffixed by compass direction (NSEW). A variety of separators are accepted (eg 3º 37' 09"W)
+# or fixed-width format without separators (eg 0033709W). Seconds and minutes may be omitted.
 # (Note minimal validation is done).
-# 
+#
 # @param   [String|Number] Degrees or deg/min/sec in variety of formats
 # @returns [Number] Degrees as decimal number
 # @throws  ArgumentError
 
+require 'active_support'
 require 'sugar-high/numeric'
 require 'sweetloader'
 
@@ -30,7 +31,7 @@ module GeoUnits
   end
 
   def self.included(base)
-    [:Maps, :Constants, :"Converter::Units"].each do |module_name| 
+    [:Maps, :Constants, :"Converter::Units"].each do |module_name|
       module_name = "GeoUnits::#{module_name.to_s.camelize}".constantize
       base.send :include, module_name
       base.extend module_name
@@ -43,9 +44,9 @@ module GeoUnits
 
   (units - [:radians]).each do |unit_type|
     define_singleton_method "#{unit_type}_to" do |unit, number = 0|
-      return 0 if number <= 0        
+      return 0 if number <= 0
       unit = normalized(unit)
-      
+
       converter = GeoUnits::Maps::Meters
       from = converter.from_unit[unit_type]
       to = converter.to_unit[unit]
@@ -58,7 +59,7 @@ module GeoUnits
     unit = normalized(unit)
     # factor = GeoUnits::Converter::Units.units_per_longitude_degree(lat, unit)
     # puts "factor: #{factor} - #{unit}"
-    (GeoUnits::Maps::Earth.distance_per_latitude_degree[unit] * number.to_f) 
+    (GeoUnits::Maps::Earth.distance_per_latitude_degree[unit] * number.to_f)
   end
 
   module ClassMethods
--- lib/geo_units/converter/units.rb
+++ lib/geo_units/converter/units.rb
@@ -1,13 +1,13 @@
 module GeoUnits
   module Converter
     module Units
-      def degrees_to_radians(degrees)   
+      def degrees_to_radians(degrees)
         degrees.to_f * GeoUnits::Constants.radians_per_degree
       end
 
       def units_sphere_multiplier(units)
         units = GeoUnits.key units
-        GeoUnits::Mapsearth_radius_map[units]
+        GeoUnits::Map::Earth.radius[units]
       end
 
       def units_per_latitude_degree(units)
@@ -20,22 +20,22 @@ module GeoUnits
       end
 
       def units_per_longitude_degree(lat, units)
-        miles_per_longitude_degree = (lat * Math.cos(lat * pi_div_rad)).abs 
+        miles_per_longitude_degree = (lat * Math.cos(lat * pi_div_rad)).abs
         units = GeoUnits.key units
         miles_per_longitude_degree.miles_to(units)
-      end 
-  
+      end
+
       def earth_radius units
         units = GeoUnits.key units
-        GeoUnits::Maps.earth_radius_map[units]
+        GeoUnits::Maps::Earth.radius[units]
       end
-      
+
       def radians_ratio units
         units = GeoUnits.key units
         radians_per_degree * earth_radius(units)
       end
-  
-      extend self       
+
+      extend self
     end
   end
 end
